8d1de8e75d3ca35cc8e2d9cd827362850fc68289
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-12 14:36:04 +0530

    Broken

---
+++ server/NetworkMessage.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Net;
+
+namespace PongGame
+{
+    // Data structure used to store Packets along with their sender
+    public class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+        public DateTime ReceiveTime { get; set; }
+    }
+}
+
